a5680acb7bd9067df2efbb7a51eaf81a8db95b0b
Quanyi Wang <wang0759@algonquinlive.com>
2020-07-04 16:57:01 -0400

    update status by fixing the bug: {status}

--- src/index.js
+++ src/index.js
@@ -108,11 +108,11 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick={i => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-          <div>status</div>
+          <div>{status}</div>
           <ol>{moves}</ol>
         </div>
       </div>
